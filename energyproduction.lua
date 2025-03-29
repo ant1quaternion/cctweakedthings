9407c3b2e8dd6d7272883d5b8fdb44c24d33c0ed
ant1quaternion <alexandermatyaszek@gmail.com>
2025-03-29 14:17:24 +0000

    Update energyproduction.lua

--- energyproduction.lua
+++ energyproduction.lua
@@ -2,6 +2,7 @@ print("RUNNING")
 local monitor = peripheral.wrap("left")
 local energystorage = peripheral.wrap("back")
 local oldFE = energystorage.getEnergy()
+local FElist = {}
 while true do
     sleep(1)
     local newFE = energystorage.getEnergy()
@@ -11,6 +12,17 @@ while true do
     monitor.setCursorPos(1,1)
     monitor.write(tostring(FEdiff) .. "FE/s energy production")
     monitor.setCursorPos(1,2)
+    table.insert(FElist,FEdiff)
+    if #FElist>10 then
+        table.remove(FElist,1)
+    end
+    local FEavg = 0
+    for _, p in pairs(FElist) do
+        FEavg=FEavg+p
+    end
+    FEavg=FEavg/#FElist
+    monitor.write(tostring(FEavg) .. "FE/s energy production avg")
+    monitor.setCursorPos(1,3)
     monitor.write(tostring(newFE) .. "FE energy storage")
     oldFE = newFE
 end

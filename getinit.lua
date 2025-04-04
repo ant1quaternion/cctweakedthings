722f5e70bd96b6ce6593abf28c5213e3d590f3bb
ant1quaternion <alexandermatyaszek@gmail.com>
2025-04-04 21:53:34 +0000

    Create getinit.lua

---
+++ getinit.lua
@@ -0,0 +1,13 @@
+local a = http.get(scripttoload)
+if a then
+  print("gone through load 1")
+  local b = a.readAll()
+  if b then
+    print("gone through load 2")
+    local c = loadstring(b)
+  else
+    print("unable to read file?")
+  end
+else
+  print("file link not valid?")
+end

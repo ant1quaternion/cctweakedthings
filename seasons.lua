c4603fac3e2554a475264be3583cfe8028752065
ant1quaternion <alexandermatyaszek@gmail.com>
2025-04-04 22:19:06 +0000

    Update seasons.lua

--- seasons.lua
+++ seasons.lua
@@ -1,8 +1,8 @@
 print("awake")
---[[local monitor = peripheral.find("monitor")
+local monitor = peripheral.find("monitor")
 while true do
     monitor.clear()
     monitor.setCursorPos(1,1)
     monitor.write("test")
     sleep(10)
-end]]
+end

c50a91c5d792b3739557e611d0b26abb29bbc875
ant1quaternion <alexandermatyaszek@gmail.com>
2025-04-05 09:52:24 +0000

    Update seasons.lua
    added season detection

--- seasons.lua
+++ seasons.lua
@@ -1,12 +1,36 @@
 print("awake")
 local monitor = peripheral.find("monitor")
-local inv = peripheral.find("minecraft:chest")
+function getUnmodifiedSeason(day)
+  local h = day
+  h=h-1
+  h=h/8
+  h=h%(3*4)
+  h=math.floor(h)
+  local subs = ""
+  local season = ""
+  if h%3==0 then
+    subs = "early"
+  elseif h%3 == 1 then
+    subs = "mid"
+  elseif h%3 == 2 then
+    subs = "late"
+  end
+  if math.floor((h)/3)==0 then
+    season = "spring"
+  elseif math.floor((h)/3)==1 then
+    season = "summer"
+  elseif math.floor((h)/3)==2 then
+    season = "autumn"
+  elseif math.floor((h)/3)==3 then
+    season = "winter"
+  end
+  return subs.." "..season
+end
 while true do
   monitor.clear()
   monitor.setCursorPos(1,1)
   monitor.write(tostring(os.day("ingame")).." day(s) elapsed")
   monitor.setCursorPos(1,2)
-  local slot1 = inv.getItemDetail(1)
-  print(textutils.serialize(slot1))
+  monitor.write(getUnmodifiedSeason(os.day("ingame")))
   sleep(10)
 end

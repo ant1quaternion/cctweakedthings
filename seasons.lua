b6139d7bf977277bd485a0283c276178c98aa5b7
ant1quaternion <alexandermatyaszek@gmail.com>
2025-04-05 08:26:35 +0000

    idk

--- seasons.lua
+++ seasons.lua
@@ -1,8 +1,15 @@
 print("awake")
 local monitor = peripheral.find("monitor")
+local inv = peripheral.find("minecraft:chest")
+function getSeason(day) 
+  day/=8
+end
 while true do
-    monitor.clear()
-    monitor.setCursorPos(1,1)
-    monitor.write("test")
-    sleep(10)
+  monitor.clear()
+  monitor.setCursorPos(1,1)
+  monitor.write(tostring(os.day("ingame")).." day(s) elapsed")
+  monitor.setCursorPos(1,2)
+  local slot1 = inv.getItemDetail(1)
+  print(textutils.serialize(slot1))
+  sleep(10)
 end

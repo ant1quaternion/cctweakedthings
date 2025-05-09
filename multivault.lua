ef1d721e9cebb9e8696108499000a2d463631080
flipflop360hover <flipflop360hover@gmail.com>
2025-05-09 19:41:47 +0100

    make me see the audio file as text

--- multivault.lua
+++ multivault.lua
@@ -17,7 +17,7 @@ end)
 coroutine.resume(co)
 modem.transmit(1,1,{["from"]="host",["to"]="all",["data"]=code})
 local speaker = peripheral.find("speaker") or print("Speaker must be attached for the funny")
-local funny = http.get("https://raw.githubusercontent.com/ant1quaternion/cctweakedthings/refs/heads/main/votv_startup.wav").readAll()
+local funny = http.get("https://raw.githubusercontent.com/ant1quaternion/cctweakedthings/refs/heads/main/votv_startup.txt").readAll()
 if speaker and funny ~= nil and funny ~= "" then
     local decoder = dfpwm.make_decoder()
     local maxlen = 16*1024

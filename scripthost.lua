1bfc5e01e52d99cd4d772f4e43665dd0a0d700b6
ant1quaternion <alexandermatyaszek@gmail.com>
2025-05-09 16:57:14 +0000

    Create scripthost.lua

---
+++ scripthost.lua
@@ -0,0 +1,19 @@
+local modem = peripheral.find("modem") or error("No modem attached", 0)
+local UUID = modem.getNameLocal()
+local process = nil
+modem.open(1)
+modem.transmit(1,1,{["from"]=UUID,["to"]="host",["data"]="connect"})
+local kill = false
+while true do
+    event, side, frequency, replyFrequency, message, distance = os.pullEvent("modem_message")
+    if process then
+        kill = true
+        coroutine.resume(process)
+        process = nil
+    end
+    if message["to"]==UUID then
+        kill = false
+        process = coroutine.create(loadstring("if kill then return end "..message.data))
+        coroutine.resume(process)
+    end
+end

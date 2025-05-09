94dced7a42121ff3bc07403c29b1783c57684323
ant1quaternion <alexandermatyaszek@gmail.com>
2025-05-09 17:21:15 +0000

    Update multivault.lua

--- multivault.lua
+++ multivault.lua
@@ -1,1 +1,29 @@
+--https://github.com/ant1quaternion/cctweakedthings/blob/main/votv_startup.wav
+local modem = peripheral.find("modem") or error("No modem attached", 0)
+local UUID = modem.getNameLocal()
+local process = nil
+local code = http.get("https://raw.githubusercontent.com/ant1quaternion/cctweakedthings/refs/heads/main/multivaulthost.lua").readAll()
+modem.open(1)
+local co = coroutine.create(function()
+    modem.transmit(1,1,{["from"]=UUID,["to"]="host",["data"]="connect"})
+    while true do
+        event, side, frequency, replyFrequency, message, distance = os.pullEvent("modem_message")
+        if message.to == "host" then
+            modem.transmit(1,1,{["from"]="host",["to"]=message.from,["data"]=code})
+        end
+    end
+end)
+co.resume()
+modem.transmit(1,1,{["from"]="host",["to"]="all",["data"]=code})
+local speaker = peripheral.find("speaker") or print("Speaker must be attached for the funny")
+local funny = http.get("https://raw.githubusercontent.com/ant1quaternion/cctweakedthings/refs/heads/main/votv_startup.wav").readAll()
+if speaker and funny ~= nil and funny ~= "" then
+    local dfpwm = require("cc.audio.dfpwm")
 
+    local decoder = dfpwm.make_decoder()
+        local buffer = decoder(funny)
+
+        while not speaker.playAudio(buffer) do
+            os.pullEvent("speaker_audio_empty")
+        end
+end
